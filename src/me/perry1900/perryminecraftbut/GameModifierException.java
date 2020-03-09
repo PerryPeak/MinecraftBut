efb05dfe78ca00e26d9eb6e60f0538b24a74df8f
PerryPeak <42516148+PerryPeak@users.noreply.github.com>
2020-03-09 23:07:50 +0200

    Deleted unused class

--- src/me/perry1900/perryminecraftbut/GameModifierException.java
+++
@@ -1,7 +0,0 @@
-package me.perry1900.perryminecraftbut;
-
-public class GameModifierException extends Exception {
-    public GameModifierException(String message){
-        super(message);
-    }
-}
